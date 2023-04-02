f199ba0c0f2dd0a41c3ec68a5acec62c803d4a02
Sam <sam.camhi@gmail.com>
2023-04-02 15:02:44 -0400

    Passed Array Filter Method

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+const findMatching = (collection, name) => {
+    let nameMatch = collection.filter(search => search.toLowerCase() === name.toLowerCase());
+    return nameMatch;
+}
+
+const fuzzyMatch = (collection, twoLetters) => {
+    let fuzzyNameMatch = collection.filter(search => (search.split("")[0] && search.split("")[1]) === (twoLetters.split("")[0] && twoLetters.split("")[1]));
+    return fuzzyNameMatch;
+}
+
+const matchName = (collection, driver) => {
+    let driverHomeMatch = collection.filter(search => search.name === driver);
+    return driverHomeMatch;
+}
